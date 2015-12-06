ff8f905fef679a0921cf8cf4d42f7d9df5ef9136
Kyle Hewitt <floofy.kh@gmail.com>
2015-12-06 14:20:16 +0000

    Got Thresholding working on the SPU. Still need to process the last chunk which wasn't a multiple of the transfer size and proved difficult to do on the spu.

--- AppleClassification/SPECode/ApplyThresholding.cpp
+++ AppleClassification/SPECode/ApplyThresholding.cpp
@@ -2,20 +2,26 @@
 #include <stdio.h>
 
 #define MAX_SIZE 512 
-#define LOGGING 1
+#define LOGGING 0
+
+#if LOGGING
+  #define DPRINTF printf
+#else
+  #define DPRINTF 
+#endif
 
 int main(vector unsigned int arg1,
 vector unsigned int arg2, 
 vector unsigned int arg3)
 {
-  printf("Reading in argument variables\n");
+  DPRINTF("Reading in argument variables\n");
   unsigned int dataAddress = spu_extract(arg1, 1); 
   unsigned int size = spu_extract(arg1, 0); 
   unsigned int lowThreshold = spu_extract(arg1, 2); 
   unsigned int highThreshold = spu_extract(arg1, 3); 
   unsigned int strongEdgeValue = spu_extract(arg2, 0); 
   unsigned int weakEdgeValue = spu_extract(arg2, 1); 
-  printf("Argument variables are:\ndataAddress:%u\nsize:%d\nthresholds:%d-%d\nedgeValues%d-%d\n", dataAddress, size, lowThreshold, highThreshold, strongEdgeValue, weakEdgeValue); 
+  DPRINTF("Argument variables are:\ndataAddress:%u\nsize:%d\nthresholds:%d-%d\nedgeValues%d-%d\n", dataAddress, size, lowThreshold, highThreshold, strongEdgeValue, weakEdgeValue); 
 
   int dataBuffer[MAX_SIZE*2] __attribute__((aligned(128)));
   int blockSize = sizeof(dataBuffer) / 2;
@@ -23,24 +29,24 @@ vector unsigned int arg3)
 
   int iterations = size / MAX_SIZE;
 
-  printf("Beginning double-buffered memory transfers and data processing, %d iterations required\n", iterations);
+  DPRINTF("Beginning double-buffered memory transfers and data processing, %d iterations required\n", iterations);
   if (iterations > 1)
   {
-    printf("Getting first memory block with tag 0\n");
-    mfc_get(dataBuffer, (int*)dataAddress, blockSize, 0, 0, 0);
+    DPRINTF("Getting first memory block with tag 0\n");
+    mfc_get(dataBuffer, dataAddress, blockSize, 0, 0, 0);
     for (int i = 1; i < iterations; ++i)
     {
-      printf("Getting %dth memory block using tag %d\n", (i+1), i&1);
+      DPRINTF("Getting %dth memory block using tag %d\n", (i+1), i&1);
       mfc_get(dataBuffer + (i & 1)*MAX_SIZE, dataAddress + i*blockSize, blockSize, i&1, 0, 0);
       
-      printf("Waiting for memory block transfer with tag %d to complete\n", 1-(i&1));
+      DPRINTF("Waiting for memory block transfer with tag %d to complete\n", 1-(i&1));
       mfc_write_tag_mask(1 << (1 - (i & 1)));
       mfc_read_tag_status_all();
-      printf("Previous transfer complete, coninuing processing\n");      
+      DPRINTF("Previous transfer complete, coninuing processing\n");      
 
       start = (i & 1) ? 0 : MAX_SIZE;
       end = start + MAX_SIZE;
-      printf("Applying thresholding to current data block\n");
+      DPRINTF("Applying thresholding to current data block\n");
       for (int j = start; j < end; ++j)
       {
         if (dataBuffer[j] > highThreshold)
@@ -56,13 +62,13 @@ vector unsigned int arg3)
           dataBuffer[j] = weakEdgeValue;
         }
       }
-      printf("Sending processed datablock back to main memory with tag %d\n", 1-(i&1));
+      DPRINTF("Sending processed datablock back to main memory with tag %d\n", 1-(i&1));
       mfc_put(dataBuffer + (1 - (i & 1))*MAX_SIZE, dataAddress + (i - 1)*blockSize, blockSize, 1 - (i & 1), 0, 0);
     }
-    printf("Waiting for last data transfer to complete\n");
+    DPRINTF("Waiting for last data transfer to complete\n");
     mfc_write_tag_mask(2);
     mfc_read_tag_status_all();
-    printf("Last data transfer complete, doing final thresholding for complete block.\n");
+    DPRINTF("Last data transfer complete, doing final thresholding for complete block.\n");
     start = MAX_SIZE;
     end = 2 * MAX_SIZE;
     for (int j = start; j < end; ++j)
@@ -80,19 +86,24 @@ vector unsigned int arg3)
         dataBuffer[j] = weakEdgeValue;
       }
     }
-    printf("Sendin processed data back to main memory\n");
-    mfc_put(dataBuffer + MAX_SIZE, (dataAddress + iterations - 1)*blockSize, blockSize, 1, 0, 0);
+    DPRINTF("Sending processed data back to main memory\n");
+    mfc_put(dataBuffer + MAX_SIZE, dataAddress + (iterations - 1)*blockSize, blockSize, 1, 0, 0);
     mfc_read_tag_status_all();
   }
 
+/*
   //If the data's size is not a multiple of 4096
   if (iterations*size != MAX_SIZE)
   {
     int remainder = size%MAX_SIZE;
-    printf("Data size is not a multiple of 4096, doing final segment\n");
-    mfc_get(dataBuffer, dataAddress + size - remainder, remainder, 0, 0, 0);
-
-    for (int j = 0; j < remainder; ++j)
+    int paddedRemainder = remainder + (remainder%128);
+    DPRINTF("Data size is not a multiple of %d, doing final segment of %d bytes\n", MAX_SIZE, remainder);
+    mfc_get(dataBuffer, dataAddress + size - paddedRemainder, paddedRemainder, 0, 0, 0);
+    DPRINTF("Waiting for data transfer to complete");
+    mfc_write_tag_mask(1<<0);
+    mfc_read_tag_status_all();
+    DPRINTF("Performing thresholding");
+    for (int j = remainder%16; j < paddedRemainder; ++j)
     {
       if (dataBuffer[j] > highThreshold)
       {
@@ -107,10 +118,12 @@ vector unsigned int arg3)
         dataBuffer[j] = weakEdgeValue;
       }
     }
-
-    mfc_put(dataBuffer, dataAddress + size - remainder, remainder, 0, 0, 0);
+    DPRINTF("Sending data back");
+    mfc_put(dataBuffer, dataAddress + size - paddedRemainder, paddedRemainder, 0, 0, 0);
+    DPRINTF("Waiting for transfer to complete");
+    mfc_read_tag_status_all();
   }
-
+*/
   return 0;
 }
 
